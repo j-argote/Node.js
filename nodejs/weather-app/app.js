5e0a86e6f1a6cfc3c0736446012abb5dddb30554
John <argote.john@gmail.com>
2019-11-05 18:35:26 -0600

    first http request

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,5 +1,8 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/90c6b702f679d77d345156a0e00cd0be/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/90c6b702f679d77d345156a0e00cd0be/37.8267,-122.4233';
 
-request({url: url}, )
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
