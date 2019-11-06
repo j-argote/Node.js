49f4407f930f6649ad40110c8bfc498e07a05258
John <argote.john@gmail.com>
2019-11-05 20:17:42 -0600

    challenge complete

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -2,7 +2,10 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/90c6b702f679d77d345156a0e00cd0be/37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.currently)
+    const currentTemp = response.body.currently.temperature
+    const currentPrecipProb = response.body.currently.precipProbability
+
+    return console.log(`It is currently ${currentTemp} degrees out. There is a ${currentPrecipProb} chance of rain.`)
 })
