9a93eab138716f3a0f9cfc4d4b723e5d61890c2a
John <argote.john@gmail.com>
2019-11-06 22:46:59 -0600

    update

--- nodejs/playground/4-callbacks.js
+++ nodejs/playground/4-callbacks.js
@@ -23,15 +23,6 @@
 // })
 
 
-
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-
 const add = (num1, num2, callback) => {
     setTimeout(() => {
         const sum = num1 + num2 
