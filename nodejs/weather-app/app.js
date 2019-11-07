1f8661624b78abfa5d2e16f1d1ad37491415ac8a
John <argote.john@gmail.com>
2019-11-07 02:42:03 -0600

    updated weather app

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+const geocode = require('./utils/geocode')
+
 // const url = 'https://api.darksky.net/forecast/90c6b702f679d77d345156a0e00cd0be/37.8267,-122.4233';
 
 // request({ url: url, json: true }, (error, response) => {
@@ -15,17 +17,23 @@ const request = require('request');
 //     }
 // })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXRvZ3JhMSIsImEiOiJjazJtb3JoOTgwazdrM25wbnEyNTVmbnB5In0.kyBEyR4BHXi9PoyBFOLkmQ&limit=1';
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/houston.json?access_token=pk.eyJ1IjoiZXRvZ3JhMSIsImEiOiJjazJtb3JoOTgwazdrM25wbnEyNTVmbnB5In0.kyBEyR4BHXi9PoyBFOLkmQ&limit=1';
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services!')
-    } else if (response.body.features.length === 0) {
-        console.log('Invalid location entered!')
-    } else {
-        const long = response.body.features[0].center[0]
-        const lat = response.body.features[0].center[1]
-        console.log(long, lat)    
-    }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location services!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Invalid location entered!')
+//     } else {
+//         const longitude = response.body.features[0].center[0]
+//         const latitude = response.body.features[0].center[1]
+//         console.log(longitude, latitude)    
+//     }
 
+// })
+
+
+geocode('destin', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
