cb665a2aae857786e871bfd9b2bb50328209a209
John <argote.john@gmail.com>
2019-11-07 23:48:12 -0600

    update

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -5,7 +5,7 @@ geocode('destin', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
-
+//forecast
 forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
