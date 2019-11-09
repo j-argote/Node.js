b8774008ec0aa32e9d5b7e84d8013520df835e25
John <argote.john@gmail.com>
2019-11-09 13:11:17 -0600

    completed challenge to take user input and provide it to geocode callback function

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,10 +1,15 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
+const city = process.argv[2]
+
+if (!city) {
+    console.log('Please enter a city!')
+} else {
+    geocode(city, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
 
     forecast(data.latitude, data.longitude, (error, forecastData) => {
         if (error) {
@@ -15,4 +20,5 @@ geocode('Boston', (error, data) => {
         console.log(forecastData)
     })
 })
+}
 
