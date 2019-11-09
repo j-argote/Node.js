8c35a29fb5b187ac9c6390f52cb36976be8349f4
John <argote.john@gmail.com>
2019-11-09 10:56:10 -0600

    chaining callback functions

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,14 +1,18 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//mapbox api call
-geocode('destin', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode('Boston', (error, data) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
 
-//darksky weather api
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
