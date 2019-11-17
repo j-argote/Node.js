992c8808c1a0199b858a052fe54d4117c90472e5
John <argote.john@gmail.com>
2019-11-16 19:35:43 -0600

    integrating handlebars for dynamic web pages

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -1,13 +1,33 @@
+// Core node modules
 const path = require('path');
+// npm modules
 const express = require('express');
-
-console.log(__dirname)
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-console.log(path.join(publicDirectoryPath, '/about.html'))
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'John Argote'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'John Argote'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help page!'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
