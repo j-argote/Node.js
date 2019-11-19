8416c7e3fc8e45c96beaf8883b8c978979411122
John <argote.john@gmail.com>
2019-11-18 22:49:30 -0600

    using partials

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -2,16 +2,19 @@
 const path = require('path');
 // npm modules
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -32,7 +35,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'This is the help page!'
+        message: 'This is the help page!',
+        title: 'Help',
+        name: 'John Argote'
     })
 })
 
