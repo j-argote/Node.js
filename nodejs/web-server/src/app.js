d958033c853f9be9b0a4293833e785729c5fd99b
John <argote.john@gmail.com>
2019-11-20 08:53:17 -0600

    created 404 page

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -48,8 +48,20 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.get('', (req, res) => {
-    
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found.',
+        name: 'John Argote'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'John Argote'
+    })
 })
 
 
